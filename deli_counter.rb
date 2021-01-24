59e5e2b031f685592cce3b159113c387c7481861
Javier <joker07ln@gmail.com>
2021-01-24 16:35:57 -0500

    commit tree 394f645aecd8ca044fcbe418eb5ee740456a6e3d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def line(array)
+    if array.length >= 1
+        new_array = ""
+        array.each_with_index do |name, index|
+            index += 1
+            new_array << " #{index}. #{name}"
+        end
+        puts "The line is currently:#{new_array}"
+    else 
+        puts "The line is currently empty."
+    end       
+end
+
+def take_a_number(katz_deli, string)
+    katz_deli << string
+    katz_deli.each_with_index do |name, index|
+        index += 1
+        katz_deli << "#{index} #{string}"
+    end
+    puts "Welcome, #{string}. You are number #{index} in line."
+end
